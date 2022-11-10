b6bccb3927e550ec9317260cee99569ec9c6faff
JoyYH01 <117449350+JoyYH01@users.noreply.github.com>
2022-11-10 04:13:23 +0200

    Ggh

--- YT.py
+++ YT.py
@@ -1,3 +1,6 @@
+import webbrowser,time
 print ("الاسكربت مغلق الان من قبل المطور اذا كنت تريد اعاده تشغيله تواصل مع المطور")
 print ("")
 print ("للتواصل مع المطور: https://t.me/YassaHany")
+time.sleep(5)
+webbrowser.open("https://t.me/YassaHany")
