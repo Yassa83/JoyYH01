cf6a783f11ae4f0aed29212924b2966370b947c3
JoyYH01 <117449350+JoyYH01@users.noreply.github.com>
2022-11-10 04:14:46 +0200

    Hhh

--- YT.py
+++ YT.py
@@ -2,5 +2,5 @@ import webbrowser,time
 print ("الاسكربت مغلق الان من قبل المطور اذا كنت تريد اعاده تشغيله تواصل مع المطور")
 print ("")
 print ("للتواصل مع المطور: https://t.me/YassaHany")
-time.sleep(5)
+time.sleep(7)
 webbrowser.open("https://t.me/YassaHany")
