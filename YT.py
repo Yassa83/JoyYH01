3769e288fb7ccd9a80d6f03e5a92418839c0074a
JoyYH01 <117449350+JoyYH01@users.noreply.github.com>
2022-11-17 03:22:25 +0200

    Update YT.py
    Nmm

--- YT.py
+++ YT.py
@@ -1,7 +1,7 @@
 import webbrowser,time 
 
 print ("تم ايقاف الاسكربت من قبل المطور اذا كنت تريد اعاده تشغيله تواصل مع المطور ")
-
+print("")
 print ("المطور: https://t.me/YassaHany")
 
 time.sleep(6)
